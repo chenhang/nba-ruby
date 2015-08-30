81f5c5bd7b496490e0df859a10d82627fdc5b8f5
hang <ch1993@163.com>
2015-08-30 13:32:30 +0800

    ADD result parser

--- lib/nba_api/basic_stats.rb
+++ lib/nba_api/basic_stats.rb
@@ -28,6 +28,23 @@ module NbaApi
           yield basic_data if block_given?
         end
       end
+
+      def to_hash origin
+        result_sets = origin['resultSets']
+        {}.tap do |data_sets|
+          result_sets.each {|result_set| data_sets[result_set['name']] = parse_result_set result_set}
+        end rescue {}
+      end
+
+      def parse_result_set result_set
+        {}.tap do |data_set|
+          result_set['headers'].each_with_index { |header, index| data_set[header] = result_set['rowSet'][index] }
+        end
+      end
+
+      def update_fields fields
+        raise NotImplementedError, "#{self.class} does not implement update_fields."
+      end
     end
 
     def self.api
