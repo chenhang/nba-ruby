328b5342dfe05a7d13848c8b73af43aa2aeb204a
hang <ch1993@163.com>
2015-08-31 22:37:05 +0800

    ADD parse logic

--- lib/nba_api/stats.rb
+++ lib/nba_api/stats.rb
@@ -1,3 +1,16 @@
 class Stats < ActiveRestClient::Base
   base_url 'localhost'
+
+  def to_hash origin
+    result_sets = origin['resultSets']
+    {}.tap do |data_sets|
+      result_sets.each { |result_set| data_sets[result_set['name']] = parse_result_set result_set }
+    end rescue {}
+  end
+
+  def parse_result_set result_set
+    {}.tap do |data_set|
+      result_set['headers'].each_with_index { |header, index| data_set[header] = result_set['rowSet'][index] }
+    end
+  end
 end
