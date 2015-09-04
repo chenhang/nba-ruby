69a7d018d7cd784c446118b15e5088665863b0c7
hang <ch1993@163.com>
2015-09-04 22:08:38 +0800

    CHG refactor object

--- lib/nba_api/basic_stats.rb
+++ lib/nba_api/basic_stats.rb
@@ -1,43 +1,29 @@
 require 'active_support/inflector'
 
 module NbaApi
-  class BasicStats
-    include ActiveModel::Validations
-    include ActiveModel::Dirty
-    include ActiveModel::Serializers::JSON
+  class BasicStats < HashObject
+    def path_name
+      self.class.name.split("::").last.underscore
+    end
 
-    class << self
-      def path_name
-        name.split("::").last.underscore
-      end
+    def find id, params = {}
+      client.find(path_name, id, params)
+    end
 
-      def find id, params = {}
-        client.find(path_name, id, params)
-      end
+    def to_hash origin
+      result_sets = origin['resultSets']
+      {}.tap do |data_sets|
+        result_sets.each { |result_set| data_sets[result_set['name']] = parse_result_set result_set }
+      end rescue {}
+    end
 
-      def save data
-        # TODO: Create instance var here
-      end
+    def create data
+      self.save to_hash(data)
+    end
 
-      def parse data
-        to_hash data
-      end
-
-      def to_hash origin
-        result_sets = origin['resultSets']
-        {}.tap do |data_sets|
-          result_sets.each {|result_set| data_sets[result_set['name']] = parse_result_set result_set}
-        end rescue {}
-      end
-
-      def parse_result_set result_set
-        {}.tap do |data_set|
-          result_set['headers'].each_with_index { |header, index| data_set[header] = result_set['rowSet'][index] }
-        end
-      end
-
-      def update_fields fields
-        fields.each { |field, value| self.instance_variable_set field.underscore, value }
+    def parse_result_set result_set
+      {}.tap do |data_set|
+        result_set['headers'].each_with_index { |header, index| data_set[header] = result_set['rowSet'][index] }
       end
     end
 
@@ -49,21 +35,8 @@ module NbaApi
       {}
     end
 
-    def self.client
-      NbaApi.client
-    end
-
-    attr_writer :client
-
-    def initialize
-    end
-
     def refresh!
-      self.class.find(id)
-    end
-
-    def client
-      @client ||= self.class.client
+      self.find(id)
     end
   end
 end
--- lib/nba_api/hash_object.rb
+++ lib/nba_api/hash_object.rb
@@ -1,6 +1,11 @@
 class HashObject
-  def initialize(hash)
+  def initialize(hash={})
+    save hash unless hash.nil?
+  end
+
+  def save hash
     hash.each { |k, v| to_self k, detect(v) }
+    self
   end
 
   def detect v
@@ -15,5 +20,18 @@ class HashObject
     self.instance_variable_set("@#{k}", v)
     self.class.send(:define_method, k, proc { self.instance_variable_get("@#{k}") })
     self.class.send(:define_method, "#{k}=", proc { |v| self.instance_variable_set("@#{k}", v) })
+  end
+
+  def to_hash origin
+    result_sets = origin['resultSets']
+    {}.tap do |data_sets|
+      result_sets.each { |result_set| data_sets[result_set['name']] = parse_result_set result_set }
+    end rescue {}
+  end
+
+  def parse_result_set result_set
+    {}.tap do |data_set|
+      result_set['headers'].each_with_index { |header, index| data_set[header] = result_set['rowSet'][index] }
+    end
   end
 end
