be303646574d7b062bedea0bd83dcae6ec0ff1e6
hang <ch1993@163.com>
2015-09-27 21:26:42 +0800

    CHG remove client class

--- lib/nba_api/basic_stats.rb
+++ lib/nba_api/basic_stats.rb
@@ -3,16 +3,31 @@ class BasicStats < NbaApi::HashObject
     self.class.name.split("::").last.underscore
   end
 
-  def find params = {}
-    NbaApi::Client.find(path_name, params)
-  end
-
   def self.apis
     {}
   end
 
   def self.default_params
     {}
+  end
+
+  def get(api, params = {})
+    uri = URI.parse(api)
+    uri.query = URI.encode_www_form(params) unless params.empty?
+    http_get(uri)
+  end
+
+  def find(path, params = {})
+    nba_class = class_from_path path
+    response = get nba_class.api, params
+    nba_class.create response
+  end
+
+  def http_get(uri)
+    http = Net::HTTP.new uri.host, uri.port
+    request = Net::HTTP::Get.new uri.request_uri
+    response = http.request request
+    JSON.parse(response.body) rescue nil
   end
 
   def refresh!
